b15567dd5e66cc9ceb1aa980d6ad150ed9865d63
Jerrod <jerrod.spiceland@mykenzie.snhu.edu>
2023-08-01 01:33:34 -0500

    form works, but does not go away

--- main.js
+++ main.js
@@ -231,51 +231,63 @@ const studentsOnDom = (array) => {
   }
   renderToDom("#app", domString);
 };
-// create sorting form
+
+
+//CREATE FORM
 const createForm = () => {
-  // create domString
-  let domString = "";
+  // target button to render
+  const buttonToShow = document.querySelector("#form-btn");
 
-  // crete form on domString
-  domString += `
-  <form id="sort-student-form">
+  // listener on click to create form
+  buttonToShow.addEventListener("click" , function () {
+    showForm.innerHTML = `<form id="sort-student-form">
     <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">What is the student's name?</label>
-    <input type="text" class="form-control" id="name" aria-describedby="name">
+      <label for="name" class="form-label">What is the student's name?</label>
+      <input type="text" class="form-control"  placeholder="name" id="name" aria-describedby="name">
     </div>
-    <button type="submit" form="sort-student-form" value="Submit">Sort</button>
-  </form>`;
+    <div class="mb-3">
+      <label for="house" class="form-label">House</label>
+      <input type="text" class="form-control" id="house"  placeholder="house" aria-describedby="houseHelp">
+    </div>
+    <div class="mb-3">
+      <input type="url" class="form-label" id="url"  placeholder="https://">
+      <label class="form-label" for="exampleCheck1">Image URL</label>
+    </div>
+    <button type="submit" id="reset-btn">Sort</button>
+  </form>`
+  });
+}
 
-  const form = document.querySelector("#sort-student-form");
 
-  //create a new student
-  const create = (e) => {
-    e.preventDefault();
-
-    // create new student object for students array of objects
-    const newStudent = {
-      id: students.length + 1,
-      name: document.querySelector("#name").value,
-    };
-
-    students.push(newStudent);
-    studentsOnDom(students);
-    form.reset();
-    form.addEventListener("submit", create);
+const create = (e) => {
+  e.preventDefault();
+  const buttonToShow = document.querySelector("#form-btn");
+  const studentForm = document.querySelector("#sort-student-form");
+  // create new student object for students array of objects
+  const newStudent = {
+    id: students.length + 1,
+    name: document.querySelector("#name").value,
+    studentPhoto: document.querySelector("#url").value,
+    house: document.querySelector("#house").value
   };
 
-  // addEventListener to sort button
-  const formBtn = document.querySelector("#form-btn");
-  formBtn.addEventListener("click", function () {
-    showForm.innerHTML = domString;
-  });
+  students.push(newStudent);
+  studentsOnDom(students);
+  studentForm.reset();
+  
 };
+
+
+
+ const form = document.querySelector("#show-form");
+ form.addEventListener("submit", create);
+
 
 const startApp = () => {
   introduction();
   createHat();
-  // housesOnDom(houses);
   studentsOnDom(students);
   createForm();
 };
+
 startApp();
