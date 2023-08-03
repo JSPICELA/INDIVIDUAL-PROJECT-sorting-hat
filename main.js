c61685d56dbcd9ccae01702ba9cad88a4e0ed850
JSPICELA <jerrod.spiceland@mykenzie.snhu.edu>
2023-08-03 05:51:41 -0500

    hmm

--- main.js
+++ main.js
@@ -115,10 +115,18 @@ const students = [
   },
 ];
 
+const firstYears = [
+  {
+    id: 1,
+    name: "Bruton Gaster",
+    studentPhoto: "https://24.media.tumblr.com/c88840c12f2fb6d23aa9af29d73fedb8/tumblr_mz4betLTJg1sycm4xo1_250.gif",
+    house: "Gryffindor"
+  },
+  
+];
+
 const app = document.querySelector("#app");
-const hat = document.querySelector("#hat");
-const introduce = document.querySelector("#introduction");
-const showForm = document.querySelector("#show-form");
+
 // Utility functions
 
 // renderToDom
@@ -127,93 +135,20 @@ const renderToDom = (divId, htmlToRender) => {
   selectedElement.innerHTML = htmlToRender;
 };
 
-//housesOnDom
-const housesOnDom = (array) => {
-  let domString = "";
-
-  for (const iterator of array) {
-    domString += `<div class="card" style="width: 18rem;">
-    <img src="${iterator.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${iterator.name}</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>`;
-  }
-  renderToDom("#app", domString);
-};
-
-// create sorting hat
-const createHat = () => {
-  let domString = "";
-  domString += `
-  <div class="card" style="width: 18rem;">
-  <img src="./asset/sorting-hat.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">I am the Sorting Hat</h5>
-    <p class="card-text">I will now sort you into your houses. The four house are... </p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Gryffindor</li>
-    <li class="list-group-item">Slytherin</li>
-    <li class="list-group-item">Hufflepuff</li>
-    <li class="list-group-item"> Ravenclaw</li>
-  </ul>
-  <div class="card-body">
-    <button id="form-btn">Begin Sorting</button>
-  </div>
-</div>`;
-
-  renderToDom("#hat", domString);
-};
-
-//introduce hat and what not
+//introduction
 const introduction = () => {
   let domString = "";
 
-  domString += `
-  <div class="container">
-    <div>
-      <h3>Welcome to Hogwarts</h3>
-      <p>Oh, you may not think I'm pretty,
-      But don't judge on what you see,
-      I'll eat myself if you can find
-      A smarter hat than me.
-      You can keep your bowlers black,
-      Your top hats sleek and tall,
-      For I'm the Hogwarts Sorting Hat
-      And I can cap them all.
-      There's nothing hidden in your head
-      The Sorting Hat can't see,
-      So try me on and I will tell you
-      Where you ought to be.
-      You might belong in Gryffindor,
-      Where dwell the brave at heart,
-      Their daring, nerve and chivalry
-      Set Gryffindors apart;
-      You might belong in Hufflepuff,
-      Where they are just and loyal,
-      Those patient Hufflepuffs are true
-      And unafraid of toil;
-      Or yet in wise old Ravenclaw,
-      If you've a ready mind,
-      Where those of wit and learning,
-      Will always find their kind;
-      Or perhaps in Slytherin
-      You'll make your real friends,
-      Those cunning folk use any means
-      To achieve their ends.
-      So put me on! Don't be afraid!
-      And don't get in a flap!
-      You're in safe hands (though I have none)
-      For I'm a Thinking Cap!</p>
-    </div>
-  </div>
-  `;
+  domString += `<h5 class="introduction">Welcome to Hogwarts</h5>
+  <hr>`
 
-  renderToDom("#introduce", domString);
-};
+  renderToDom("#app" , domString);
+}
+
+//begin sort button
+const createSortButton = () => {
+  app.innerHTML += `<button class="btn btn-primary" id="begin-sort-btn">Begin Sort</button>`
+}
 
 //show students on dom
 const studentsOnDom = (array) => {
@@ -229,65 +164,215 @@ const studentsOnDom = (array) => {
     </div>
   </div>`;
   }
-  renderToDom("#app", domString);
+  app.innerHTML += domString;
 };
-// create sorting form
+
+//create form
 const createForm = () => {
-  // create domString
+//xx
+
+  app.innerHTML += `<div class="container">
+  <form id="sort-student-form">
+  
+    <div class>
+      <label for="name" class="form-label"><h3>Enter First Year's Name</h3></label>
+    </div>
+    <div>
+    <br>
+      <label>Student:</label>
+      <input type"text" placeholder="Barny Weasley" id="name">
+      <button type="submit" id="sort-btn" class="btn btn-primary">Sort</button>
+      </div>
+
+      <div>
+        <label>Student Photo:</label>
+        <input type="url" class="form-label" id="url"  placeholder="https://">
+        <br>
+        <div class="filter-buttons">
+        <button type="button" class="btn btn-light" id ="all">All</button>
+        <button type="button" class="btn btn-danger" id="gryffindor">Gryffindor</button>
+        <button type="button" class="btn btn-warning" id="hufflepuff">Hufflepuff</button>
+        <button type="button" class="btn btn-primary" id="ravenclaw">Ravenclaw</button>
+        <button type="button" class="btn btn-success" id="slytherin">Slytherin</button>
+        <button type="button" class="btn btn-dark" id="death-eaters">Death Eaters</button>
+        </div>
+        <hr>
+      </div>
+   </div>
+  </form>`;
+  const form = document.querySelector("#sort-student-form");
+    form.reset();
+    form.addEventListener("submit", createStudents);
+
+    const gryffindorBtn = document.querySelector("#gryffindor");
+    gryffindorBtn.addEventListener("click" , function () {
+      let domString = "";
+
+      for (const student of firstYears) {
+        if (firstYears.house === "Gryffindor"){
+          domString += `<div class="card" style="width: 18rem;">
+        <div class= "card-body">
+          <h5 class= "card-title">${student.name}</h5>
+          <img src=${student.studentPhoto} class="card-img-top" >
+          <p class="card-text">${student.house}</p>
+          <button type="button" class="btn btn-danger">Expel</button>
+        </div>`;
+        }
+        app.innerHTML = domString;
+      }
+    });
+
+    const allBtn = document.querySelector("#all");
+allBtn.addEventListener("click" , function () {
   let domString = "";
 
-  // crete form on domString
-  domString += `
-  <form id="sort-student-form">
-    <div class="mb-3">
-      <label for="name" class="form-label">What is the student's name?</label>
-      <input type="text" class="form-control" id="name" aria-describedby="name">
-    </div>
-    <div class="mb-3">
-      <label for="house" class="form-label">House</label>
-      <input type="text" class="form-control" id="house" aria-describedby="houseHelp">
-    </div>
-    <div class="mb-3">
-      <input type="url" class="form-label" id="url">
-      <label class="form-label" for="exampleCheck1">Image URL</label>
-    </div>
-    <button type="submit" form="sort-student-form" value="Submit">Sort</button>
-  </form>`;
-  
-  const form = document.querySelector("#sort-student-form");
-  // addEventListener to sort button
-  const formBtn = document.querySelector("#form-btn");
-  formBtn.addEventListener("click", function () {
-    showForm.innerHTML = domString;
-  });
+  for (const student of firstYears) {
+    
+      domString += `<div class="card" style="width: 18rem;">
+    <div class= "card-body">
+      <h5 class= "card-title">${student.name}</h5>
+      <img src=${student.studentPhoto} class="card-img-top" >
+      <p class="card-text">${student.house}</p>
+      <button type="button" class="btn btn-danger">Expel</button>
+    </div>`;
+    }
+    renderToDom("#app" , domString);
+  }
+);
 
-  //create a new student
-  const create = (e) => {
-    e.preventDefault();
+const hufflepuffBtn = document.querySelector("#hufflepuff");
+hufflepuffBtn.addEventListener("click" , function () {
+  let domString = "";
 
-    // create new student object for students array of objects
-    const newStudent = {
-      id: students.length + 1,
-      name: document.querySelector("#name").value,
-      studentPhoto: document.querySelector("#url").value,
-      house: document.querySelector("#house").value
-    };
+  for (const student of firstYears) {
+    if (firstYears.house === "Hufflepuff"){
+      domString += `<div class="card" style="width: 18rem;">
+    <div class= "card-body">
+      <h5 class= "card-title">${student.name}</h5>
+      <img src=${student.studentPhoto} class="card-img-top" >
+      <p class="card-text">${student.house}</p>
+      <button type="button" class="btn btn-danger">Expel</button>
+    </div>`;
+    }
+    app.innerHTML = domString;
+  }
+});
+const ravenclawBtn = document.querySelector("#ravenclaw");
+ravenclawBtn.addEventListener("click" , function () {
+  let domString = "";
 
-    students.push(newStudent);
-    studentsOnDom(students);
-    form.reset();
+  for (const student of firstYears) {
+    if (firstYears.house === "Ravenclaw"){
+      domString += `<div class="card" style="width: 18rem;">
+    <div class= "card-body">
+      <h5 class= "card-title">${student.name}</h5>
+      <img src=${student.studentPhoto} class="card-img-top" >
+      <p class="card-text">${student.house}</p>
+      <button type="button" class="btn btn-danger">Expel</button>
+    </div>`;
+    }
+    app.innerHTML = domString;
+  }
+});
+const slytherinBtn = document.querySelector("#slytherin");
+slytherinBtn.addEventListener("click" , function () {
+  let domString = "";
+
+  for (const student of firstYears) {
+    if (firstYears.house === "Slytherin"){
+      domString += `<div class="card" style="width: 18rem;">
+    <div class= "card-body">
+      <h5 class= "card-title">${student.name}</h5>
+      <img src=${student.studentPhoto} class="card-img-top" >
+      <p class="card-text">${student.house}</p>
+      <button type="button" class="btn btn-danger">Expel</button>
+    </div>`;
+    }
+    app.innerHTML = domString;
+  }
+});
+const deathEaterBtn = document.querySelector("#death-eaters");
+deathEaterBtn.addEventListener("click" , function () {
+  let domString = "";
+
+  for (const student of firstYears) {
+    if (firstYears.house === "Death Eater"){
+      domString += `<div class="card" style="width: 18rem;">
+    <div class= "card-body">
+      <h5 class= "card-title">${student.name}</h5>
+      <img src=${student.studentPhoto} class="card-img-top" >
+      <p class="card-text">${student.house}</p>
+      <button type="button" class="btn btn-danger">Expel</button>
+    </div>`;
+    }
+    app.innerHTML = domString;
+  }
+});
+   
+}
+
+//create filter buttons
+const createFilterButtons = () => {
+  app.innerHTML += 
+  `
+  <div class="filter-buttons">
+  <button type="button" class="btn btn-light" id ="all">All</button>
+  <button type="button" class="btn btn-danger" id="gryffindor">Gryffindor</button>
+  <button type="button" class="btn btn-warning" id="hufflepuff">Hufflepuff</button>
+  <button type="button" class="btn btn-primary" id="ravenclaw">Ravenclaw</button>
+  <button type="button" class="btn btn-success" id="slytherin">Slytherin</button>
+  <button type="button" class="btn btn-dark" id="death-eaters">Death Eaters</button>
+  </div>
+  `
+}
+
+//random number  function
+function randomNumber (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+ }
+
+// randomHouse function
+function randomHouse () {
+  if (randomNumber(1,4) === 1) {
+    return "Slytherin"
+  } else if (randomNumber(1,4) === 2) {
+    return "Gryffindor"
+  } else if (randomNumber(1,4) === 3) {
+    return "Hufflepuff"
+  } else {
+    return "Ravenclaw"
+  }
+}
+
+//create students
+const createStudents = (e) => {
+  e.preventDefault();
+
+  const newStudent = {
+    id: firstYears.length + 1,
+    name: document.querySelector("#name").value,
+    studentPhoto: document.querySelector("#url").value,
+    house: randomHouse()
   };
-  
-  form.addEventListener("submit", create);
- 
-};
 
+  firstYears.push(newStudent);
+  studentsOnDom(firstYears);
+}
+
+//add event listeners
+const addListeners = () => {
+  const sortBtn = document.querySelector("#begin-sort-btn");
+  sortBtn.addEventListener("click" , createForm);
+
+
+
+}
+
+// launch app
 const startApp = () => {
   introduction();
-  createHat();
-   housesOnDom(houses);
-  studentsOnDom(students);
-  createForm();
-};
+  createSortButton();
+  addListeners();
+}
 
 startApp();
