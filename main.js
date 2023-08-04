e7d1496852e3a36e0a34f92ba0c73c19da55546f
JSPICELA <jerrod.spiceland@mykenzie.snhu.edu>
2023-08-04 06:48:52 -0500

    can add new students

--- main.js
+++ main.js
@@ -119,10 +119,10 @@ const firstYears = [
   {
     id: 1,
     name: "Bruton Gaster",
-    studentPhoto: "https://24.media.tumblr.com/c88840c12f2fb6d23aa9af29d73fedb8/tumblr_mz4betLTJg1sycm4xo1_250.gif",
-    house: "Gryffindor"
+    studentPhoto:
+      "https://24.media.tumblr.com/c88840c12f2fb6d23aa9af29d73fedb8/tumblr_mz4betLTJg1sycm4xo1_250.gif",
+    house: "Gryffindor",
   },
-  
 ];
 
 const app = document.querySelector("#app");
@@ -140,15 +140,16 @@ const introduction = () => {
   let domString = "";
 
   domString += `<h5 class="introduction">Welcome to Hogwarts</h5>
-  <hr>`
+  <hr>`;
 
-  renderToDom("#app" , domString);
-}
+  renderToDom("#app", domString);
+};
 
 //begin sort button
 const createSortButton = () => {
-  app.innerHTML += `<button class="btn btn-primary" id="begin-sort-btn">Begin Sort</button>`
-}
+  app.innerHTML += `<button class="btn btn-primary" id="begin-sort-btn">Begin Sort</button>`;
+  
+};
 
 //show students on dom
 const studentsOnDom = (array) => {
@@ -164,14 +165,14 @@ const studentsOnDom = (array) => {
     </div>
   </div>`;
   }
-  app.innerHTML += domString;
+  app.innerHTML = domString;
 };
 
 //create form
 const createForm = () => {
-//xx
+  //xx
 
-  app.innerHTML += `<div class="container">
+ app.insertAdjacentHTML('beforebegin' , `<div class="container">
   <form id="sort-student-form">
   
     <div class>
@@ -199,35 +200,34 @@ const createForm = () => {
         <hr>
       </div>
    </div>
-  </form>`;
+  </form>`);
   const form = document.querySelector("#sort-student-form");
-    form.reset();
-    form.addEventListener("submit", createStudents);
+  
+  form.addEventListener("submit", createStudents);
 
-    const gryffindorBtn = document.querySelector("#gryffindor");
-    gryffindorBtn.addEventListener("click" , function () {
-      let domString = "";
+  const gryffindorBtn = document.querySelector("#gryffindor");
+  gryffindorBtn.addEventListener("click", function () {
+    let domString = "";
 
-      for (const student of firstYears) {
-        if (firstYears.house === "Gryffindor"){
-          domString += `<div class="card" style="width: 18rem;">
+    for (const student of firstYears) {
+      if (firstYears.house === "Gryffindor") {
+        domString += `<div class="card" style="width: 18rem;">
         <div class= "card-body">
           <h5 class= "card-title">${student.name}</h5>
           <img src=${student.studentPhoto} class="card-img-top" >
           <p class="card-text">${student.house}</p>
           <button type="button" class="btn btn-danger">Expel</button>
         </div>`;
-        }
-        app.innerHTML = domString;
       }
-    });
+      app.innerHTML = domString;
+    }
+  });
 
-    const allBtn = document.querySelector("#all");
-allBtn.addEventListener("click" , function () {
-  let domString = "";
+  const allBtn = document.querySelector("#all");
+  allBtn.addEventListener("click", function () {
+    let domString = "";
 
-  for (const student of firstYears) {
-    
+    for (const student of firstYears) {
       domString += `<div class="card" style="width: 18rem;">
     <div class= "card-body">
       <h5 class= "card-title">${student.name}</h5>
@@ -236,85 +236,82 @@ allBtn.addEventListener("click" , function () {
       <button type="button" class="btn btn-danger">Expel</button>
     </div>`;
     }
-    renderToDom("#app" , domString);
-  }
-);
+    renderToDom("#app", domString);
+  });
 
-const hufflepuffBtn = document.querySelector("#hufflepuff");
-hufflepuffBtn.addEventListener("click" , function () {
-  let domString = "";
+  const hufflepuffBtn = document.querySelector("#hufflepuff");
+  hufflepuffBtn.addEventListener("click", function () {
+    let domString = "";
 
-  for (const student of firstYears) {
-    if (firstYears.house === "Hufflepuff"){
-      domString += `<div class="card" style="width: 18rem;">
+    for (const student of firstYears) {
+      if (firstYears.house === "Hufflepuff") {
+        domString += `<div class="card" style="width: 18rem;">
     <div class= "card-body">
       <h5 class= "card-title">${student.name}</h5>
       <img src=${student.studentPhoto} class="card-img-top" >
       <p class="card-text">${student.house}</p>
       <button type="button" class="btn btn-danger">Expel</button>
     </div>`;
+      }
+      app.innerHTML = domString;
     }
-    app.innerHTML = domString;
-  }
-});
-const ravenclawBtn = document.querySelector("#ravenclaw");
-ravenclawBtn.addEventListener("click" , function () {
-  let domString = "";
+  });
+  const ravenclawBtn = document.querySelector("#ravenclaw");
+  ravenclawBtn.addEventListener("click", function () {
+    let domString = "";
 
-  for (const student of firstYears) {
-    if (firstYears.house === "Ravenclaw"){
-      domString += `<div class="card" style="width: 18rem;">
+    for (const student of firstYears) {
+      if (firstYears.house === "Ravenclaw") {
+        domString += `<div class="card" style="width: 18rem;">
     <div class= "card-body">
       <h5 class= "card-title">${student.name}</h5>
       <img src=${student.studentPhoto} class="card-img-top" >
       <p class="card-text">${student.house}</p>
       <button type="button" class="btn btn-danger">Expel</button>
     </div>`;
+      }
+      app.innerHTML = domString;
     }
-    app.innerHTML = domString;
-  }
-});
-const slytherinBtn = document.querySelector("#slytherin");
-slytherinBtn.addEventListener("click" , function () {
-  let domString = "";
+  });
+  const slytherinBtn = document.querySelector("#slytherin");
+  slytherinBtn.addEventListener("click", function () {
+    let domString = "";
 
-  for (const student of firstYears) {
-    if (firstYears.house === "Slytherin"){
-      domString += `<div class="card" style="width: 18rem;">
+    for (const student of firstYears) {
+      if (firstYears.house === "Slytherin") {
+        domString += `<div class="card" style="width: 18rem;">
     <div class= "card-body">
       <h5 class= "card-title">${student.name}</h5>
       <img src=${student.studentPhoto} class="card-img-top" >
       <p class="card-text">${student.house}</p>
       <button type="button" class="btn btn-danger">Expel</button>
     </div>`;
+      }
+      app.innerHTML = domString;
     }
-    app.innerHTML = domString;
-  }
-});
-const deathEaterBtn = document.querySelector("#death-eaters");
-deathEaterBtn.addEventListener("click" , function () {
-  let domString = "";
+  });
+  const deathEaterBtn = document.querySelector("#death-eaters");
+  deathEaterBtn.addEventListener("click", function () {
+    let domString = "";
 
-  for (const student of firstYears) {
-    if (firstYears.house === "Death Eater"){
-      domString += `<div class="card" style="width: 18rem;">
+    for (const student of firstYears) {
+      if (firstYears.house === "Death Eater") {
+        domString += `<div class="card" style="width: 18rem;">
     <div class= "card-body">
       <h5 class= "card-title">${student.name}</h5>
       <img src=${student.studentPhoto} class="card-img-top" >
       <p class="card-text">${student.house}</p>
       <button type="button" class="btn btn-danger">Expel</button>
     </div>`;
+      }
+      app.innerHTML = domString;
     }
-    app.innerHTML = domString;
-  }
-});
-   
-}
+  });
+};
 
 //create filter buttons
 const createFilterButtons = () => {
-  app.innerHTML += 
-  `
+  app.innerHTML += `
   <div class="filter-buttons">
   <button type="button" class="btn btn-light" id ="all">All</button>
   <button type="button" class="btn btn-danger" id="gryffindor">Gryffindor</button>
@@ -323,56 +320,55 @@ const createFilterButtons = () => {
   <button type="button" class="btn btn-success" id="slytherin">Slytherin</button>
   <button type="button" class="btn btn-dark" id="death-eaters">Death Eaters</button>
   </div>
-  `
-}
+  `;
+};
 
 //random number  function
-function randomNumber (min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
- }
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 // randomHouse function
-function randomHouse () {
-  if (randomNumber(1,4) === 1) {
-    return "Slytherin"
-  } else if (randomNumber(1,4) === 2) {
-    return "Gryffindor"
-  } else if (randomNumber(1,4) === 3) {
-    return "Hufflepuff"
+function randomHouse() {
+  if (randomNumber(1, 4) === 1) {
+    return "Slytherin";
+  } else if (randomNumber(1, 4) === 2) {
+    return "Gryffindor";
+  } else if (randomNumber(1, 4) === 3) {
+    return "Hufflepuff";
   } else {
-    return "Ravenclaw"
+    return "Ravenclaw";
   }
 }
 
 //create students
 const createStudents = (e) => {
   e.preventDefault();
-
+  const form = document.querySelector("#sort-student-form");
   const newStudent = {
     id: firstYears.length + 1,
     name: document.querySelector("#name").value,
     studentPhoto: document.querySelector("#url").value,
-    house: randomHouse()
+    house: randomHouse(),
   };
 
   firstYears.push(newStudent);
+  form.reset();
   studentsOnDom(firstYears);
-}
+  console.log("click");
+};
 
 //add event listeners
 const addListeners = () => {
   const sortBtn = document.querySelector("#begin-sort-btn");
-  sortBtn.addEventListener("click" , createForm);
-
-
-
-}
+  sortBtn.addEventListener("click", createForm);
+};
 
 // launch app
 const startApp = () => {
   introduction();
   createSortButton();
   addListeners();
-}
+};
 
 startApp();
